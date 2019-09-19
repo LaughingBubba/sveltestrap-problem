2a5ce9b2775076a26dcc0dc7865191235dd1b1cb
LaughingBubba <joseph.szili@gmail.com>
2019-09-19 20:06:55 +1000

    add comment about mxing plain BS4 and sveltestrap

--- src/Home.svelte
+++ src/Home.svelte
@@ -5,6 +5,10 @@
 <ul>
     <li>Open the console before clicking on the button below ...</li>
 </ul>
+
+<!-- If you uncomment the below it will still fail as though it were a sveltestrap button -->
+<!-- <button type="button" class="btn btn-success" on:click={()=>navigate('/hello')} >Say Hello</button> -->
+
 <Button color='warning' on:click={()=>navigate('/hello')} >Say Hello</Button>
 
 <script>
