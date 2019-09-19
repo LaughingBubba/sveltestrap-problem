e139d2663d7a8da0bf7b18535174055f1ef3b8ac
LaughingBubba <joseph.szili@gmail.com>
2019-09-19 19:31:17 +1000

    initial set up

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+	import Hello from './Hello.svelte'
+	import Home from './Home.svelte'
+	import {Router, Route, Link, navigate} from 'svelte-routing'
+</script>
+
+<Router>
+	<Route exact path="/" component={Home} />
+	<Route exact path="/hello">
+		<Hello name={"Mundo"}/>
+	</Route>
+</Router>
---
+++ src/Home.svelte
@@ -0,0 +1,15 @@
+<h2>Home!</h2>
+
+<p>Welcome to this sample code!</p>
+<p>Things to try:</p>
+<ul>
+    <li>Navigate around with ... Yadda yadda yadda</li>
+</ul>
+<Button color='warning' on:click={()=>navigate('/hello')} >Say Hello</Button>
+
+<script>
+
+  import { navigate } from 'svelte-routing'
+  import { Button } from 'sveltestrap'
+
+</script>
